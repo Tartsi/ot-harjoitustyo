de0e0fdedeb23b0d653f2e0975a1be4ac6a0d67d
Tartsi <tarvo.lilja@hotmail.fi>
2022-04-12 22:58:54 +0300

    Viimeinen commimt laskinsovellukseen liittyen

--- src/calculator.py
+++ src/calculator.py
@@ -3,9 +3,9 @@ from tkinter import *
 class Calculator:
 
     # Luodaan uusi luokkamuuttuja laskimen muotoiluun liittyen
-    
+
     color_light_gray = "#D3D3D3"
-    
+
     # Alustetaan laskimelle pääikkuna ja määritetään sen koko vakioksi, annetaan ikkunalle myös nimi
 
     def __init__(self):
@@ -23,8 +23,8 @@ class Calculator:
         
         self.display_area, self.button_area = self.create_areas()
 
-    # Luodaan funktio, joka palauttaa "vastausalueen" ja nappialueen
-    
+    # Luodaan funktio, joka luo ja palauttaa "vastausalueen" ja nappialueen
+
     def create_areas(self):
         display_frame = Frame(self.screen, height=200, bg=self.color_light_gray)
         display_frame.pack(expand=False, fill="both")
@@ -33,16 +33,6 @@ class Calculator:
         return display_frame, button_frame
 
     # Funktio laskimen käynnistykselle
-    
+
     def run(self):
         self.screen.mainloop()
-
-# --- Väliaikainen osa koodia ---
-
-def main():
-    calculator = Calculator()
-    calculator.screen.update_idletasks()
-    calculator.run()
-    
-if __name__ == '__main__':
-    main()
